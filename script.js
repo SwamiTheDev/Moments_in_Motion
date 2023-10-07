fb012f1264ea75cf2938a4bf81f3f114baca56b0
swamithedev@gmail.com <swamithedev@gmail.com>
2023-10-07 13:00:57 +0530

    base script file added

---
+++ script.js
@@ -0,0 +1,10 @@
+//from dynamic calendar js
+dycalendar.draw({
+    target: "#dycalendar",
+    type: "month",
+    dayformat: "full",
+    monthformat: "full",
+    highlighttargetdate: true,
+    prevnextbutton: "show",
+  });
+  
