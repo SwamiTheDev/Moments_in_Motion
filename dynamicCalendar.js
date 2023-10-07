e951b57db8dae06ad9d616d9b59bfd83a1e6c97e
swamithedev@gmail.com <swamithedev@gmail.com>
2023-10-07 13:18:08 +0530

    Add the libary license

---
+++ dynamicCalendar.js
@@ -0,0 +1,14 @@
+/*!
+ * dyCalendar is a JavaScript library for creating Calendar.
+ *
+ * Author: Yusuf Shakeel
+ * https://github.com/yusufshakeel
+ *
+ * GitHub Link: https://github.com/yusufshakeel/dyCalendarJS
+ *
+ * MIT license
+ * Copyright (c) 2016 Yusuf Shakeel
+ *
+
+ */
+/*! dyCalendarJS | (c) 2016 Yusuf Shakeel | https://github.com/yusufshakeel/dyCalendarJS */
