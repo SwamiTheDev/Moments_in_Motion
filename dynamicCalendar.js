f474fa675ca5e5127f8c75229bdb0217c712ea48
swamithedev@gmail.com <swamithedev@gmail.com>
2023-10-07 13:19:24 +0530

    set the variable

--- dynamicCalendar.js
+++ dynamicCalendar.js
@@ -12,3 +12,62 @@
 
  */
 /*! dyCalendarJS | (c) 2016 Yusuf Shakeel | https://github.com/yusufshakeel/dyCalendarJS */
+
+(function (global) {
+    "use strict";
+  
+    var //this will be used by the user.
+      dycalendar = {},
+      //window document
+      document = global.document,
+      //starting year
+      START_YEAR = 1900,
+      //end year
+      END_YEAR = 9999,
+      //name of the months
+      monthName = {
+        full: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ],
+        mmm: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+      },
+      //name of the days
+      dayName = {
+        full: [
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+        ],
+        d: ["S", "M", "T", "W", "T", "F", "S"],
+        dd: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+        ddd: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      };
+    })  
